234608104f1e174b07570cc8a5ee8c72ae6ad5d0
vsaraikin <vsaraikin@splunk.com>
2023-09-24 10:54:55 +0200

    added kafka-service

---
+++ kafka-service/go.mod
@@ -0,0 +1,14 @@
+module kafka-tutorial
+
+go 1.20
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/segmentio/kafka-go v0.4.42
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
+)
+
+require (
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+)
---
+++ kafka-service/main2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+	fmt.Println(brokers)
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		GroupID:  groupID,
+		Topic:    topic,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+}
+
+func main() {
+	// get kafka reader using environment variables.
+	kafkaURL := "localhost:9092"
+	topic := "quickstart-events"
+	groupID := "group"
+
+	reader := getKafkaReader(kafkaURL, topic, groupID)
+
+	defer reader.Close()
+
+	fmt.Println("start consuming ... !!")
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+	}
+}
